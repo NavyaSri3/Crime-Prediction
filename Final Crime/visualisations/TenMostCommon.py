695b1f7c916d620634947b7f9f65ecd58a9eb03b
NavyaSri3 <46093787+NavyaSri3@users.noreply.github.com>
2019-03-16 10:48:59 +0530

    Add files via upload

---
+++ Final Crime/visualisations/TenMostCommon.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+train = pd.read_csv('../data/train.csv', header=0)
+
+ten_most_common = train[train['Category'].isin(train['Category'].value_counts().head(10).index)]
+
+ten_most_crime_by_district = pd.crosstab(ten_most_common['PdDistrict'], ten_most_common['Category'])
+ten_most_crime_by_district.plot(kind='barh', figsize=(16,10), stacked=True, colormap='Greens',
+                                title='Distribution of the City-wide Ten Most Common Crimes in Each District')
+plt.show()
